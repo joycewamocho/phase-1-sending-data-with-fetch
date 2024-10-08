3b137057c992579d92021a0249b6591fcd345c09
joyce wamocho <joywamo@gmail.com>
2024-10-08 18:08:37 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers:{
+            "content-Type": "application/json",
+            "Accept":  "application/json",
+        },
+        body:JSON.stringify({
+            name:name,
+            email:email
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) =>{
+        const newId = data.id
+        const p = document.createElement("p")
+        p.textContent =`The user ID is: ${newId}`
+        document.querySelector("body").appendChild(p)
+    })
+    .catch((error) =>{
+        const p =document.createElement("p")
+        p.textContent = error.message
+        document.querySelector("body").appendChild(p)
+        
+    })
+
+}
+submitData("Joyce", "joywamo@gmail.com");
